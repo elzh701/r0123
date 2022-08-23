6f693d97a40286102f125c0a1a5de2a6c135e17f
elzh701 <elzh701@gmail.com>
2022-08-23 19:01:51 +0300

    Примеры

---
+++ ex01.py
@@ -0,0 +1,12 @@
+from pprint import pprint
+
+num = 5
+num_str = "5"
+print("print")
+print(num)
+print(num_str)
+
+pprint("pprint")
+pprint(num)
+pprint(num_str)
+
---
+++ tusk_01.py
@@ -0,0 +1,8 @@
+a = 50
+b = 10
+c = 3
+
+
+print(a)
+print(b)
+
